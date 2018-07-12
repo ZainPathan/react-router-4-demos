0ea2d5d71055faac0b5ab98738465737b2dc9999
ZainPathan <zainpathan@gmail.com>
2018-07-12 14:29:05 +0530

    Basic React Router setup with some simple routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+
+import Home from './components/HomeComponent';
+import Hello from './components/HelloComponent';
+import About from './components/AboutComponent';
+import Books from './components/BooksComponent';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <Router>
+        <div className="App">
+          <div className='container'>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/hello'>Hello</Link></li>
+              <li><Link to='/about'>About</Link></li>
+              <li><Link to='/books'>Books</Link></li>
+            </ul>
+          </div>
+
+          <Route path='/' exact={true} component={Home} />
+          <Route path='/hello' component={Hello} />
+          <Route path='/about' component={About} />
+          <Route path='/books' component={Books} />
+
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
+      </Router>
     );
   }
 }
